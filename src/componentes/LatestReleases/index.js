e1a504f034073eba871dda60beab479bbfc80ecc
Eduardo Poletti <eduardopoletti96@gmail.com>
2024-12-10 10:44:54 -0300

    Last day

--- src/componentes/LatestReleases/index.js
+++ src/componentes/LatestReleases/index.js
@@ -1,6 +1,8 @@
 import { books } from "../LatestReleases/dataLatestReleases";
 import styled from "styled-components";
 import {Title} from "../Title";
+import RecomendationCard from "../RecomendationCard";
+import bookImage from "../../images/livro2.png" 
 
 const LatestReleasesContainer = styled.section`
     background-color: #EBECEE;
@@ -23,12 +25,20 @@ function LatestReleases() {
             <Title 
                 color="#EB9B00" 
                 fontSize="36px" 
-            >LATEST RELEASES</Title>
+            >
+                LATEST RELEASES
+            </Title>
                 <NewBooksContainer>
                     {books.map( book => (
                         <img src={book.src}/>
                     ))}
                 </NewBooksContainer>
+                <RecomendationCard
+                    title="Maybe you will like"
+                    subtitle="Angular 11"
+                    description="Building an application with Google Platform"
+                    image={bookImage}
+                ></RecomendationCard>
         </LatestReleasesContainer>
     )
 }
